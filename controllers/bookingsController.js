52f5257ac1d258d05ab6db285b3a7518503a42da
EarlMisury <lucash.santini@gmail.com>
2021-07-06 18:47:38 -0300

    Fixing environment variable bug

--- controllers/bookingsController.js
+++ controllers/bookingsController.js
@@ -81,6 +81,7 @@ exports.tourIsBooked = catchAsync(async (req, res, next) => {
 
 exports.checkTourOccupancy = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
+  console.log('$CHECKTOUROCCUPANCYTOUR$', tour);
   const tourBookings = await Booking.aggregate([
     {
       $match: { tour: tour._id },
@@ -117,6 +118,7 @@ exports.checkTourOccupancy = catchAsync(async (req, res, next) => {
     .map((el) => new Date(el.date).getTime());
 
   const bookedDate = Number(req.params.tourDate);
+  console.log('$CHECKTOUROCCUPANCYTOURDATE$', bookedDate, tourBookings);
 
   if (tourBookings.length !== 0 && fullDates.includes(bookedDate))
     return next(new AppError('Selected date is full, try another', 400));
@@ -142,6 +144,8 @@ const createBookingCheckout = async (session) => {
   const tourDate = session.client_reference_id.split('/')[1];
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].amount / 100;
+
+  console.log('##', tour, tourDate, user, price);
   await Booking.create({
     tour,
     user,
